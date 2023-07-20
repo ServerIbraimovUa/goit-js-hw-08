53bfbce29f72974035801f79c3eadd576990e72a
ServerIbraimovUa <serveribraimov7@gmail.com>
2023-07-20 16:56:08 +0300

    first commit

---
+++ src/js/02-video.js
@@ -0,0 +1,10 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', onPlay);
+
+function onPlay(data) {
+  console.log(data);
+}

72ae42b8572c888acd7256227b5f0200005b82f0
ServerIbraimovUa <serveribraimov7@gmail.com>
2023-07-21 10:28:51 +0300

    video add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,16 @@
 import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+console.log(throttle);
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', onPlay);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-function onPlay(data) {
-  console.log(data);
+function getCurrentTime(currentTime) {
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(currentTime.seconds));
 }
+player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-console.log('hello');
+const currentValue = localStorage.getItem(CURRENT_TIME_KEY);
+player.setCurrentTime(JSON.parse(currentValue));
